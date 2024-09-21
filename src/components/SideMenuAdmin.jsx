9e562e9fc43c282b68490eebd40edf5317deb47a
GastonZ <gastonzappullaryu@gmail.com>
2024-09-21 16:04:34 -0300

    -

--- src/components/SideMenuAdmin.jsx
+++ src/components/SideMenuAdmin.jsx
@@ -32,7 +32,7 @@ const SideMenuAdmin = () => {
     return (
         <div className='flex flex-col items-center bg-gray-300 py-4 w-[400px] h-screen'>
             <h1 className='bg-black p-2 rounded-lg w-[150px] font-medium text-center text-white text-xl'>Administrador</h1>
-            <div className='flex flex-col gap-3 mt-8'>
+            <div className='flex flex-col gap-3 my-8'>
                 {adminItems.map((x) => (
                     <button className='bg-primary-blue p-2 rounded-xl font-semibold text-white'>
                         {x.text}
