c6733a590c2be1df1cf8404a0be51c3de57a2833
GastonZ <gastonzappullaryu@gmail.com>
2024-09-29 11:21:07 -0300

    --

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -146,12 +146,14 @@ const Home = () => {
       <main className='flex justify-center items-center bg-background-navy p-8 h-screen'>
         <section className='lg:block relative hidden w-1/2'>
           <img className='rounded-full w-[500px] h-[500px] object-cover' src={icons.tucuman} alt="Imágen de Tucumán" />
-          <img className='xl:block top-[250px] right-[300px] absolute hidden rounded-full w-[350px] h-[350px] object-cover' src={icons.tucuman} alt="Imágen de Tucumán" />
+          <img className='xl:block top-[250px] right-[300px] absolute hidden rounded-full w-[350px] h-[350px] object-cover' src={icons.casaHistorica} alt="Imágen de Tucumán" />
         </section>
         <section className='flex flex-col justify-center items-center gap-6 h-screen'>
           <h1 className='text-[60px] text-center sm:text-[90px]'>Bienvenidos a <br /> <strong className='uppercase'>Tucumán</strong> </h1>
           <h2 className='text-3xl'>Comenzar a planear tu viaje</h2>
-          <button className='bg-primary-blue hover:bg-primary-lightBlue px-7 py-2 rounded-2xl font-medium text-3xl transition-all'>Iniciar sesión</button>
+          <Link to={'/iniciar'}>
+            <button className='bg-primary-blue hover:bg-primary-lightBlue px-7 py-2 rounded-2xl font-medium text-3xl transition-all'>Iniciar sesión</button>
+          </Link>
           <p className='text-2xl text-center'>
             ¿No te encuentras registrado?
             <Link to={'/registro'} className='hover:text-primary-darkBlue transition cursor-pointer'>
