71648f1e5bb3034f2058bad2fcce8ddf9158de93
GastonZ <gastonzappullaryu@gmail.com>
2025-02-24 19:16:16 -0300

    user blind show option menu correctly

--- src/pages/Options/Options.jsx
+++ src/pages/Options/Options.jsx
@@ -1,65 +1,34 @@
-import React from 'react'
-import { icons } from '../../assets'
-import { Link } from 'react-router-dom'
-import { Battery100Icon, ChatBubbleBottomCenterIcon, HeartIcon, MapIcon, PencilIcon } from '@heroicons/react/20/solid'
+import React, { useState, useEffect } from 'react';
+import { icons } from '../../assets';
+import { Link } from 'react-router-dom';
+import { Battery100Icon, ChatBubbleBottomCenterIcon, HeartIcon, MapIcon, PencilIcon } from '@heroicons/react/20/solid';
 
 const Options = () => {
+    const [currentUserBlind, setCurrentUserBlind] = useState(localStorage.getItem('isNoVidente') === "true");
 
-    const currentUserBlind = localStorage.getItem('isNoVidente') || null;
+    useEffect(() => {
+        const handleStorageChange = () => {
+            setCurrentUserBlind(localStorage.getItem('isNoVidente') === "true");
+        };
+
+        window.addEventListener("storage", handleStorageChange);
+        return () => window.removeEventListener("storage", handleStorageChange);
+    }, []);
 
     const buttonArray = [
-        {
-            text: 'Realizar plan de viaje',
-            icon: <PencilIcon className='w-10 h-10' />,
-            href: '/calendario'
-        },
-        {
-            text: 'Puntos turisticos & eventos',
-            icon: <MapIcon className='w-10 h-10' />,
-            href: '/puntos-turisticos'
-        },
-        {
-            text: 'Mis viajes',
-            icon: <HeartIcon className='w-10 h-10' />,
-            href: '/misviajes'
-        },
-        {
-            text: 'Puntos de carga',
-            icon: <Battery100Icon className='w-10 h-10' />,
-            href: '/puntos-de-carga'
-        },
-        {
-            text: 'Consultar a chatbot',
-            icon: <ChatBubbleBottomCenterIcon className='w-10 h-10' />,
-            href: '/chatbot'
-        },
-    ]
+        { text: 'Realizar plan de viaje', icon: <PencilIcon className='w-10 h-10' />, href: '/calendario' },
+        { text: 'Puntos turisticos & eventos', icon: <MapIcon className='w-10 h-10' />, href: '/puntos-turisticos' },
+        { text: 'Mis viajes', icon: <HeartIcon className='w-10 h-10' />, href: '/misviajes' },
+        { text: 'Puntos de carga', icon: <Battery100Icon className='w-10 h-10' />, href: '/puntos-de-carga' },
+        { text: 'Consultar a chatbot', icon: <ChatBubbleBottomCenterIcon className='w-10 h-10' />, href: '/chatbot' },
+    ];
 
     const buttonArrayBlind = [
-        {
-            text: 'Realizar plan de viaje',
-            icon: <PencilIcon className='w-10 h-10' />,
-            href: '/calendario'
-        },
-        {
-            text: 'Puntos turisticos & eventos',
-            icon: <MapIcon className='w-10 h-10' />,
-            href: '/puntos-turisticos'
-        },
-        {
-            text: 'Consultar a chatbot',
-            icon: <ChatBubbleBottomCenterIcon className='w-10 h-10' />,
-            href: '/chatbot'
-        },
-        {
-            text: 'Guia Turistica',
-            icon: <ChatBubbleBottomCenterIcon className='w-10 h-10' />,
-            href: '/guia-turistica'
-        },
-    ]
+        { text: 'Guia Turistica', icon: <ChatBubbleBottomCenterIcon className='w-10 h-10' />, href: '/guia-turistica' },
+    ];
 
     return (
-        !currentUserBlind ?
+        currentUserBlind ? (
             <main className='flex flex-wrap justify-center items-center gap-10 min-h-screen'>
                 <div className="absolute flex flex-wrap justify-center gap-8">
                     {buttonArrayBlind.map((x, index) => (
@@ -72,7 +41,7 @@ const Options = () => {
                     ))}
                 </div>
             </main>
-            :
+        ) : (
             <main className="flex flex-col items-center bg-background-navy min-h-screen">
                 <img className="relative p-8 rounded-lg w-full h-[300px] lg:h-[700px] object-cover" src={icons.cabildo} alt="" />
                 <div className="top-[20%] lg:top-2/3 absolute flex flex-wrap justify-center gap-8">
@@ -86,7 +55,8 @@ const Options = () => {
                     ))}
                 </div>
             </main>
-    )
-}
+        )
+    );
+};
 
-export default Options
+export default Options;
